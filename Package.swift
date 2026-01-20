a6410f78b8c7eebc7c7f7de2672011e203729842
daironian <daironigr@gmail.com>
2026-01-20 11:35:56 +0100

    init

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "IconsMinds",
+    platforms: [
+        .iOS(.v14), .macOS(.v11), .tvOS(.v14), .watchOS(.v7)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,8 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "IconsMinds"
+            name: "IconsMinds",
+            resources: [
+                .process("Resources/icomoon.ttf")
+            ]
         ),
-
+        
     ]
 )
--- Sources/IconsMinds/IconsMinds.swift
+++ Sources/IconsMinds/IconsMinds.swift
@@ -1,2 +1,32 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct IconsMindsView: View {
+    private let icon: IconsMinds?
+    private let size: CGFloat
+    private let fontName: String
+    
+    public init(_ icon: IconsMinds, size: CGFloat = 20, fontName: String = "Icons-Minds") {
+        self.icon = icon
+        self.size = size
+        self.fontName = fontName
+    }
+    
+    public init(_ icon: String, size: CGFloat = 20, fontName: String = "Icons-Minds") {
+        if let iconEnum = IconsMinds(name: icon) {
+            self.icon = iconEnum
+        }else{
+            self.icon = nil
+        }
+        self.size = size
+        self.fontName = fontName
+    }
+    
+    public var body: some View {
+        if let icon = icon {
+            Text(icon.unicodeScalar)
+                .font(.custom(fontName, size: size))
+        }
+    }
+}
